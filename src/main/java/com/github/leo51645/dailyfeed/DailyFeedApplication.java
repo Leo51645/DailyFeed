76d5306beffd3e9227d545115c7d44bc2bc901b1
Leo51645 <fischer.leonard@gmx.de>
2026-05-28 18:22:28 +0200

    initial commit

---
+++ src/main/java/com/github/leo51645/dailyfeed/DailyFeedApplication.java
@@ -0,0 +1,13 @@
+package com.github.leo51645.dailyfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyFeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/leo51645/dailyfeed/DailyFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.leo51645.dailyfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyFeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
