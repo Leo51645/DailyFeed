b79f02892e54e3fddd42eec1b1e3c6b04ab2f11a
Leo51645 <fischer.leonard@gmx.de>
2026-05-28 23:29:18 +0200

    feat: add selected assets as an enum with symbol as parameter

---
+++ src/main/java/com/github/leo51645/dailyfeed/domain/enums/Assets.java
@@ -0,0 +1,24 @@
+package com.github.leo51645.dailyfeed.domain.enums;
+
+public enum Assets {
+    APPLE("AAPL"),
+    MICROSOFT("MSFT"),
+    GOOGLE("GOOGL"), // alternativ auch GOOG
+    AMAZON("AMZN"),
+    META("META"),
+    NVIDIA("NVDA"),
+    TESLA("TSLA"),
+    SandP_500("^GSPC"),
+    DAX("^GDAXI"),
+    NIKKEI_225("^N225"),
+    EURO_STOXX_50("^STOXX50E"),
+    GOLD("GC=F"),
+    SILVER("SI=F"),
+    BITCOIN("BTC-USD");
+
+    private final String symbol;
+
+    Assets(String symbol) {
+        this.symbol = symbol;
+    }
+}
