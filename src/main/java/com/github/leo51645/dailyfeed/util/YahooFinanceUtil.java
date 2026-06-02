af7f582c97e28f6e61efeaa0c4d563f572b0dfab
Leo51645 <fischer.leonard@gmx.de>
2026-06-02 15:40:18 +0200

    fix: wrong day range in request uri

--- src/main/java/com/github/leo51645/dailyfeed/util/YahooFinanceUtil.java
+++ src/main/java/com/github/leo51645/dailyfeed/util/YahooFinanceUtil.java
@@ -8,6 +8,6 @@ public class YahooFinanceUtil {
     public String createYahooFinanceRequestURI(Assets asset) {
         return "https://query1.finance.yahoo.com/v8/finance/chart/"
                 + asset.getSymbol()
-                + "?range=5d&interval=1d&includePrePost=false&events=history&corsDomain=finance.yahoo.com";
+                + "?range=6d&interval=1d&includePrePost=false&events=history&corsDomain=finance.yahoo.com";
     }
 }
