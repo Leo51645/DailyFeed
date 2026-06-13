988fc86faa4ab75fd2ebd5d35208aff48781917d
Leo51645 <fischer.leonard@gmx.de>
2026-06-13 15:11:15 +0200

    feat: add parsing of the ai response to HashMap

--- src/main/java/com/github/leo51645/dailyfeed/service/GeminiService.java
+++ src/main/java/com/github/leo51645/dailyfeed/service/GeminiService.java
@@ -1,13 +1,24 @@
 package com.github.leo51645.dailyfeed.service;
 
 import com.github.leo51645.dailyfeed.domain.dto.response.NewsResponseDto;
+import com.github.leo51645.dailyfeed.domain.enums.News_Categories;
+import com.github.leo51645.dailyfeed.util.CurrentsNewsUtil;
 import com.github.leo51645.dailyfeed.util.GeminiServiceUtil;
 import com.google.genai.Client;
 import com.google.genai.types.GenerateContentResponse;
 import lombok.RequiredArgsConstructor;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.DateFormatter;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -15,7 +26,9 @@ public class GeminiService {
 
     private final Client client = new Client();
     private final GeminiServiceUtil geminiServiceUtil;
+    private final CurrentsNewsUtil currentsNewsUtil;
 
+    // create AI prompt out off all news, send it and get response of the gemini AI
     private GenerateContentResponse getGeminiNewsResponse(List<NewsResponseDto> firstDay, List<NewsResponseDto> secondDay, List<NewsResponseDto> today) {
         return client.models.generateContent("gemini-2.5-flash",
                 "You are a news ranking assistant. You will receive news articles from three different days, each containing multiple categories.\n" +
@@ -62,5 +75,61 @@ public class GeminiService {
                 "\n" +
                 "Today:\n" + geminiServiceUtil.NewsListToJson(today)
                 , null);
+    }
+
+    private Map<LocalDate, Map<News_Categories, List<NewsResponseDto>>> parseAIResponse(GenerateContentResponse aiResponse) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss Z");
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        String responseString = aiResponse.text();
+        JSONArray root = new JSONArray(responseString);
+
+        Map<LocalDate, Map<News_Categories, List<NewsResponseDto>>> allNews = new HashMap<>();
+
+        // for each existing date
+        for (int i = 0; i < root.length(); i++) {
+            JSONObject day = root.getJSONObject(i);
+            String date = day.getString("date");
+
+            JSONArray topNews = day.getJSONArray("topNews");
+
+            Map<News_Categories, List<NewsResponseDto>> topNewsOneDayAllCategories = new HashMap<>();
+            // for each existing category at one specific date
+            for (int j = 0; j < topNews.length(); j++) {
+                JSONObject categoryFilter = topNews.getJSONObject(j);
+
+                String responseCategory = categoryFilter.getString("category");
+                News_Categories category = currentsNewsUtil.getNewsCategoryFromResponseCategory(responseCategory);
+
+                JSONArray articles = categoryFilter.getJSONArray("articles");
+
+                List<NewsResponseDto> topNewsOneDayOneCategory = new ArrayList<>();
+
+                // for each existing article on one specific day and one category
+                for (int k = 0; k < articles.length(); k++) {
+                    JSONObject articlesJSONObject = articles.getJSONObject(k);
+
+                    String title = articlesJSONObject.getString("title");
+                    String description = articlesJSONObject.optString("description");
+                    String url = articlesJSONObject.getString("url");
+
+                    String publishedAt = articlesJSONObject.getString("publishedAt");
+                    OffsetDateTime dateTime = OffsetDateTime.parse(publishedAt, dateTimeFormatter);
+
+                    NewsResponseDto newsArticle = NewsResponseDto.builder()
+                            .title(title)
+                            .description(description)
+                            .url(url)
+                            .category(category)
+                            .publishedAt(dateTime)
+                            .build();
+
+                    topNewsOneDayOneCategory.add(newsArticle);
+                }
+                topNewsOneDayAllCategories.put(category, topNewsOneDayOneCategory);
+            }
+            allNews.put(OffsetDateTime.parse(date, dateFormatter).toLocalDate(), topNewsOneDayAllCategories);
+        }
+        return allNews;
     }
 }
