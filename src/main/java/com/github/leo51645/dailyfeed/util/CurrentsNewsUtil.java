67915d0c6da474f07f08abba76649f7298ec3b44
Leo51645 <fischer.leonard@gmx.de>
2026-06-14 01:12:02 +0200

    fix: 'unknown category' bug by checking the ai response for both the categoryName and the enum name

--- src/main/java/com/github/leo51645/dailyfeed/util/CurrentsNewsUtil.java
+++ src/main/java/com/github/leo51645/dailyfeed/util/CurrentsNewsUtil.java
@@ -9,25 +9,27 @@ public class CurrentsNewsUtil {
     public String createCurrentsNewsRequestURI(News_Categories news_category, String apiKey, String startDate, String endDate, int pageNumber) {
         return "https://api.currentsapi.services/v2/search?apiKey=" +
                 apiKey + "&language=de&category=" +
-                news_category.getCategoryName() + "&start_date=2026-06-13T00:00:00Z&end_date=2026-06-13T23:59:59Z" +
+                news_category.getCategoryName() + "&start_date=2026-06-11T00:00:00Z&end_date=2026-06-11T23:59:59Z" +
                 "&domain_not=noz.de,bild.de,kleinezeitung.de,merkur.de,sueddeutsche.de,tt.com,focus.de,taz.de,cicero.de," +
                 "nachdenkseiten.de,rollingstone.de,golem.de,presseportal.de,woz.ch,blick.ch,hna.de,watson.de,manager-magazin.de," +
-                "t3n.de,heise.de,spektrum.de,hardwareluxx.de,blog.gwup.net" +
+                "t3n.de,heise.de,spektrum.de,hardwareluxx.de,blog.gwup.net,handelsblatt.com,dev.to,diepresse.com,computerbild.de,nzz.ch," +
+                "kleinezeitung.at,globenewswire.com,tagesanzeiger.ch,prnewswire.com,orf.at,bernerzeitung.ch,ooe.orf.at,tirol.orf.at,steiermark.orf.at,sport.orf.at," +
+                "kurier.at,miamiherald.com,markets.ft.com" +
                 "&page_number=" + pageNumber;
     }
 
     public News_Categories getNewsCategoryFromResponseCategory(String responseCategory) {
         News_Categories category = null;
 
-        if (responseCategory.equals(News_Categories.ECONOMY.getCategoryName())) {
+        if (responseCategory.equals(News_Categories.ECONOMY.name()) || responseCategory.equals(News_Categories.ECONOMY.getCategoryName())) {
             category = News_Categories.ECONOMY;
-        } else if (responseCategory.equals(News_Categories.SOCIETY.getCategoryName())) {
+        } else if (responseCategory.equals(News_Categories.SOCIETY.name()) || responseCategory.equals(News_Categories.SOCIETY.getCategoryName())) {
             category = News_Categories.SOCIETY;
-        } else if (responseCategory.equals(News_Categories.POLITICS.getCategoryName())) {
+        } else if (responseCategory.equals(News_Categories.POLITICS.name()) || responseCategory.equals(News_Categories.POLITICS.getCategoryName())) {
             category = News_Categories.POLITICS;
-        } else if (responseCategory.equals(News_Categories.SPORTS.getCategoryName())) {
+        } else if (responseCategory.equals(News_Categories.SPORTS.name()) || responseCategory.equals(News_Categories.SPORTS.getCategoryName())) {
             category = News_Categories.SPORTS;
-        } else if (responseCategory.equals(News_Categories.TECHNOLOGY_SCIENCE.getCategoryName())) {
+        } else if (responseCategory.equals(News_Categories.TECHNOLOGY_SCIENCE.name()) || responseCategory.equals(News_Categories.TECHNOLOGY_SCIENCE.getCategoryName())) {
             category = News_Categories.TECHNOLOGY_SCIENCE;
         }
 
