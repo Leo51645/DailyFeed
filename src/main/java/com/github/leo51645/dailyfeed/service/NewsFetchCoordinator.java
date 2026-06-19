9dc8c2b291740442606f4e2b43701cdbd36f54b9
Leo51645 <fischer.leonard@gmx.de>
2026-06-19 18:50:42 +0200

    feat: add 30 min cooldown before fetching again

--- src/main/java/com/github/leo51645/dailyfeed/service/NewsFetchCoordinator.java
+++ src/main/java/com/github/leo51645/dailyfeed/service/NewsFetchCoordinator.java
@@ -8,6 +8,7 @@ import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,14 +21,22 @@ public class NewsFetchCoordinator {
     private final CacheService cacheService;
     private final GeminiService geminiService;
 
+    private LocalDateTime lastFetch = null;
+
     private String getMissingNews(LocalDate today, Map<LocalDate, Map<News_Categories, List<NewsResponseDto>>> cachedMap) {
         String missingNews = "";
         if (!cachedMap.containsKey(today) && !cachedMap.containsKey(today.minusDays(1))) {
             missingNews =  geminiService.getNewsAllDays(today);
+            lastFetch = LocalDateTime.now();
         } else if (!cachedMap.containsKey(today)) {
             missingNews =  geminiService.getNewsTwoDays(today);
+            lastFetch = LocalDateTime.now();
         } else if (cachedMap.containsKey(today)) {
-            missingNews =  geminiService.getNewsOneDay(today);
+            if (lastFetch == null || lastFetch.plusMinutes(30).isBefore(LocalDateTime.now())) {
+                missingNews =  geminiService.getNewsOneDay(today);
+                lastFetch = LocalDateTime.now();
+            }
+
         }
         return missingNews;
     }
